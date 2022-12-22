6798e54a75684c9e104310e924e4d969419fb202
Joseph Elias <jrelias@outlook.com>
2022-12-22 00:51:20 -0500

    updates to images and functionality for hover picture change

--- build/js/main.js
+++ build/js/main.js
@@ -2,14 +2,41 @@ const initApp = () => {
   const hamburgerBtn = document.getElementById('hamburger-button');
   const mobileMenu = document.getElementById('mobile-menu');
 
+  const homeDragon = document.getElementById('homeDragon');
+  const charizard = document.getElementById('charizard');
+  const drogon = document.getElementById('drogon');
+  const dragon = document.getElementById('dragon');
+
   const toggleMenu = () => {
     mobileMenu.classList.toggle('hidden');
     mobileMenu.classList.toggle('flex');
     hamburgerBtn.classList.toggle('toggle-btn');
   };
 
+  const toggleCharizard = () => {
+    homeDragon.src = './img/realisticCharizard.png';
+  };
+
+  const toggleDrogon = () => {
+    homeDragon.src = './img/drogon.png';
+  };
+
+  const toggleDragon = () => {
+    homeDragon.src = './img/dragonCage.png';
+  };
+
+  const toggleGold = () => {
+    homeDragon.src = './img/chestofgold.png';
+  };
+
   hamburgerBtn.addEventListener('click', toggleMenu);
   mobileMenu.addEventListener('click', toggleMenu);
+  charizard.addEventListener('mouseover', toggleCharizard);
+  charizard.addEventListener('mouseout', toggleDragon);
+  drogon.addEventListener('mouseover', toggleDrogon);
+  drogon.addEventListener('mouseout', toggleDragon);
+  dragon.addEventListener('mouseover', toggleGold);
+  dragon.addEventListener('mouseout', toggleDragon);
 };
 
 //we want to wait for all the content to load, so you are guaranteed to be able to select the buttons
